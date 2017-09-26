c048f071a3e81a7d7648ba51df4fe00dbead81be
Klayton <kliekkio@gmail.com>
2017-09-26 11:04:10 -0700

    started on structure, made DB

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheese Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mushroom Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("BBQ Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Double Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Vegan Burger", false);
