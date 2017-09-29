98d5c88d8cd142e0ff558a959b5536832b36f54b
Klayton <kliekkio@gmail.com>
2017-09-28 19:22:20 -0700

    added jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "gRack4bry",
-  database: "burgers_DB"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL); 
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'gRack4bry',
+    database: 'burgers_DB'
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
